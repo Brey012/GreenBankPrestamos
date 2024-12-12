8cc94960f6dade9c92f90ab194a386ee2f2aabfa
Breyner012 <bhernandezos@cesde.net>
2024-12-11 19:43:43 -0500

    Creación de la clase hija PrestamoEmpresarial

---
+++ src/Prestamos/PrestamoEmpresarial.java
@@ -0,0 +1,75 @@
+package Prestamos;
+
+public class PrestamoEmpresarial extends Prestamos {
+
+    private String nombreEmpresa;
+    private String sectorEcnonomico;
+    private String planNegocio;
+    private double evaluacionRiesgo;
+    private String destinoFondos;
+
+    // Constructor
+
+
+    public PrestamoEmpresarial(int id, double monto, String fechaOtorgamiento, double tasaInteres, int plazo, String nombreEmpresa, String sectorEcnonomico, String planNegocio, double evaluacionRiesgo, String destinoFondos) {
+        super(id, monto, fechaOtorgamiento, tasaInteres, plazo);
+        this.nombreEmpresa = nombreEmpresa;
+        this.sectorEcnonomico = sectorEcnonomico;
+        this.planNegocio = planNegocio;
+        this.evaluacionRiesgo = evaluacionRiesgo;
+        this.destinoFondos = destinoFondos;
+    }
+
+    // Methods
+
+    @Override
+    public double calcularCoutoMensual(double monto, double tasaInteres, int plazoMeses) {
+        return super.calcularCoutoMensual(monto, tasaInteres, plazoMeses);
+    }
+
+    @Override
+    public double calcularInteresTotal() {
+        return super.calcularInteresTotal();
+    }
+
+    // Getters and Setters
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getSectorEcnonomico() {
+        return sectorEcnonomico;
+    }
+
+    public void setSectorEcnonomico(String sectorEcnonomico) {
+        this.sectorEcnonomico = sectorEcnonomico;
+    }
+
+    public String getPlanNegocio() {
+        return planNegocio;
+    }
+
+    public void setPlanNegocio(String planNegocio) {
+        this.planNegocio = planNegocio;
+    }
+
+    public double getEvaluacionRiesgo() {
+        return evaluacionRiesgo;
+    }
+
+    public void setEvaluacionRiesgo(double evaluacionRiesgo) {
+        this.evaluacionRiesgo = evaluacionRiesgo;
+    }
+
+    public String getDestinoFondos() {
+        return destinoFondos;
+    }
+
+    public void setDestinoFondos(String destinoFondos) {
+        this.destinoFondos = destinoFondos;
+    }
+}
